dca7d088cf1dd90319fade252e72eec05742d2e5
Comma Llama <lbyxiafei@gmail.com>
2023-07-16 12:47:30 -0700

    Check point, 2023-07-16T12:47:30.

--- src/vscripts/abilities/heroes/builder/infi_ability_build.ts
+++ src/vscripts/abilities/heroes/builder/infi_ability_build.ts
@@ -7,13 +7,6 @@ export class infi_ability_build extends BaseAbility
 
     GetCooldown() {
         let cooldown = this.GetSpecialValueFor("cooldown");
-        if (IsServer()) {
-            const talent = this.GetCaster().FindAbilityByName("special_bonus_unique_meepo_3");
-            if (talent) {
-                cooldown -= talent.GetSpecialValueFor("value");
-            }
-        }
-
         return cooldown;
     }
 
@@ -21,7 +14,6 @@ export class infi_ability_build extends BaseAbility
         if (IsServer()) {
             this.GetCaster().EmitSound("Hero_Meepo.Earthbind.Cast");
         }
-
         return true;
     }
 
