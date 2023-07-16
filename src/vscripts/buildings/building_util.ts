b5ce7a86d2f9dc825f28498cdfced9909457799b
Comma Llama <lbyxiafei@gmail.com>
2023-07-16 14:52:37 -0700

    Minor.

--- src/vscripts/buildings/building_util.ts
+++ src/vscripts/buildings/building_util.ts
@@ -19,7 +19,6 @@ export class BuildingUtil {
     }
 
     public FindBaseEntityByPos(pos: Vector): CBaseEntity{
-        //pos.z = 168;
         const baseEntities = Entities.FindAllByClassnameWithin("trigger_dota", pos, 1) as CDOTA_BaseNPC[];
         for(const be of baseEntities){
             print("Found:" + pos);
