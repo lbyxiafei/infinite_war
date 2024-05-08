166d594f622ccdea938353724487f00b302ac952
Comma Llama <lbyxiafei@gmail.com>
2024-05-07 22:48:55 -0700

    feat: impl find tower bases

--- src/vscripts/buildings/building_util.ts
+++ src/vscripts/buildings/building_util.ts
@@ -1,19 +1,19 @@
 import { modifier_panic } from "../modifiers/modifier_panic";
 
 export class BuildingUtil {
-    constructor(){
-    }
+    constructor(){}
 
     public InitTowersBase(): void {
-        const tb = this.CreateBuildingBasedOnBaseEntityName("npc_dota_hero_morphling", "base_left_0_0");
-        const tb_origin = tb.GetOrigin()
-        tb_origin.z-=100
-        tb.SetOrigin(tb_origin)
+        const entities = this.FindAllTowerBases();
+        for(const e of entities) {
+            const tower_base = this.CreateBuildingBasedOnBaseEntityName("npc_dota_hero_morphling", e.GetName());
+            const tb_origin = tower_base.GetOrigin()
+            tb_origin.z-=100
+            tower_base.SetOrigin(tb_origin)
+        }
     }
 
-    public ReplaceTower(): void {
-
-    }
+    public ReplaceTower(): void {}
 
     public CreateBuildingBasedOnPos(
         buildingName: string, 
@@ -23,6 +23,23 @@ export class BuildingUtil {
         const entity = this.FindBaseEntityByPos(pos) as CBaseEntity;
         const unit = this.CreateBuildingBasedOnBaseEntity(buildingName, entity);
         return unit;
+    }
+
+    public FindAllTowerBases(): CBaseEntity[]{
+        // todo(binyan.li)
+        const entities = Entities.FindAllByClassnameWithin("trigger_dota", Vector(), 10000) as CBaseEntity[];
+        let res:CBaseEntity[] = []
+        for(const e of entities) {
+            if(e.GetName().startsWith("base_left_0") 
+            || e.GetName().startsWith("base_left_1") 
+            || e.GetName().startsWith("base_left_2")
+            || e.GetName().startsWith("base_right_0")
+            || e.GetName().startsWith("base_right_1")
+            || e.GetName().startsWith("base_right_2")) {
+                res.push(e)
+            }
+        }
+        return res
     }
 
     public FindBaseEntityByPos(pos: Vector): CBaseEntity{
